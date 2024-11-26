a88c74b596279b5cd154625165ab24b287885dd7
MCGallaspy <gallaspy.michael@gmail.com>
2024-11-25 16:02:37 -0800

    Tweak display, add ctls

--- streamlit_apps/win_rate_plotter.py
+++ streamlit_apps/win_rate_plotter.py
@@ -26,30 +26,81 @@ if not display_list:
     st.stop()
 
 @st.cache_data
-def get_figure(df, display_list, ycol):
+def get_figure(df, display_list, ycol, plot_means=False):
     fig = plt.figure(figsize=(8, 4))
     ax = plt.gca()
     mask = df.pair.isin(display_list)
+    
+    if plot_means:
+        average_win_rate = df[mask].win_rate.mean()
+        ax.hlines(
+            average_win_rate,
+            xmin=df.week.min()-1, xmax=df.week.max()+1,
+            colors="gray",
+            label=f"Average\nwin rate\nfor set ({average_win_rate:.1f}%)",
+            linestyles="dashed",
+            zorder=-1,
+            linewidths=2,
+        )
+    
     sns.lineplot(
         data=df[mask],
         x="week",
         y=ycol,
         style="pair",
         hue="pair",
+        linewidth=3,
     )
     sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    
+    buffer = 0.5
+    ax.set_xticks(range(1, df.week.max()+1))
+    ax.set_xlim((1-buffer, df.week.max()+buffer))
+    
     return fig, ax
 
-fig, ax = get_figure(df, display_list, "win_rate")
-ax.set_ylim((0, 100))
-ax.set_yticks([i * 10 for i in range(11)])
-ax.set_yticklabels([f"{i * 10}%" for i in range(11)])
+cols = st.columns(3)
+with cols[0]:
+    fixed = st.toggle("Fixed y-axis", value=False)
+    st.markdown("Constrain the y-axis to these values. When disabled the limits are determined automatically.")
+with cols[1]:
+    ymin = st.number_input(
+        "Y min",
+        0,
+        100,
+        value=0,
+        disabled=not fixed,
+    )
+with cols[2]:
+    ymax = st.number_input(
+        "Y max",
+        0,
+        100,
+        value=100,
+        disabled=not fixed,
+    )
+
+fig, ax = get_figure(df, display_list, "win_rate", plot_means=True)
+if fixed:
+    ax.set_ylim((ymin, ymax))
+yticks = ax.get_yticks()
+ax.set_yticklabels([f"{i}%" for i in yticks])
 ax.set_title("Win rates over weeks of Regulation H")
+
 st.write(fig)
 
 fig, ax = get_figure(df, display_list, "appearances")
 ax.set_title("Appearances over weeks of Regulation H")
 st.write(fig)
 
-st.markdown("""**Note:** The data comes from Pokemon Showdown Reg H Bo1 and Bo3 replays with a rating >= 1300.
-""")
+st.markdown("**Note:** The data comes from Pokemon Showdown Reg H Bo1 and Bo3 replays with a rating >= 1300.")
+
+if st.toggle("Show data table", value=False):
+    if st.toggle("Average over all weeks", value=True):
+        st.write(df.groupby(by="pair").agg({
+            "win_rate": ["mean", "median", "std"],
+            "players": "sum",
+            "week": "count",
+        }))
+    else:
+        st.write(df[df.pair.isin(display_list)])
